5254b33fb2084ae195edf862b243a897beb974bf
Korshunov Igor <igor.9206@gmail.com>
2023-07-19 17:14:23 +0600

    add fun numericFormat

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -18,32 +18,34 @@ class MainActivity : AppCompatActivity() {
             "21 мая в 18:36",
             "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
             false,
-            0,
-            0
+            999,
+            9995
         )
 
         with(binding) {
             author.text = post.author
             published.text = post.published
             content.text = post.content
-            countLikes.text = post.likes.toString()
-            countShare.text = post.share.toString()
+            countLikes.text = post.numericFormat(post.likes)
+            countShare.text = post.numericFormat(post.share)
 
             if (post.likedByMe) {
                 likes.setImageResource(R.drawable.ic_liked_24)
             }
 
             likes.setOnClickListener {
+                println("likes")
                 post.likedByMe = !post.likedByMe
                 if (post.likedByMe) post.likes++ else post.likes--
                 likes.setImageResource(if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_baseline_favorite_border_24)
-                countLikes.text = post.likes.toString()
+                countLikes.text = post.numericFormat(post.likes)
             }
 
             share.setOnClickListener {
                 post.share++
-                countShare.text = post.share.toString()
+                countShare.text = post.numericFormat(post.share)
             }
+
         }
 
     }
