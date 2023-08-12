120d8905ffcf671ad7e1d36bfff16d6b979ccaff
Korshunov Igor <igor.9206@gmail.com>
2023-08-12 09:17:37 +0600

    add onBackPressedDispatcher

--- app/src/main/java/ru/netology/nmedia/activity/EditPostActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/EditPostActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import ru.netology.nmedia.databinding.ActivityEditPostBinding
 import ru.netology.nmedia.util.AndroidUtils.focusAndShowKeyboard
 
@@ -31,6 +32,19 @@ class EditPostActivity : AppCompatActivity() {
             }
             finish()
         }
+
+        val activity = this
+        activity.onBackPressedDispatcher.addCallback(
+            activity, object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    val content = intent.getStringExtra(Intent.ACTION_EDIT)
+                    val intent = Intent()
+                    intent.putExtra(Intent.EXTRA_TEXT, content)
+                    setResult(Activity.RESULT_OK, intent)
+                    finish()
+                }
+            }
+        )
 
     }
 }
